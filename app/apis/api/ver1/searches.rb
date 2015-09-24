0a1755b7f37847fc95970a5734e5ca6a9b6b1dfb
himkt <himkt@klis.tsukuba.ac.jp>
2015-09-25 07:44:45 +0900

    implement optional search

--- app/apis/api/ver1/searches.rb
+++ app/apis/api/ver1/searches.rb
@@ -7,7 +7,14 @@ module API
       resource :search do
         desc 'simple search API'
         get do
-          {"count"=> Subject.all.size}
+
+          query = ''
+          params.each_with_index do |(key, value), index|
+            query += "#{key} like '%#{value}%'"
+            query += " AND " if params.size-1 != index
+          end
+
+          puts Subject.where(query)
         end
       end
     end
