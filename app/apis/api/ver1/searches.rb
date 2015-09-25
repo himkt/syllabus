af9acbc89e4c43d5b4d142611f11b8a32896ab0a
himkt <himkt@klis.tsukuba.ac.jp>
2015-09-25 09:05:16 +0900

    implement search API

--- app/apis/api/ver1/searches.rb
+++ app/apis/api/ver1/searches.rb
@@ -4,17 +4,26 @@ module API
   module Ver1
     class Searches < Grape::API
 
+      # query type init
+      normal_fields = ['scode', 'unit', 'grade', 'credit']
+      like_fields = ['sname', 'location', 'semester', 'time', 'lecturer', 'summary', 'note']
+
       resource :search do
         desc 'simple search API'
         get do
 
-          query = ''
-          params.each_with_index do |(key, value), index|
-            query += "#{key} like '%#{value}%'"
-            query += " AND " if params.size-1 != index
-          end
+          begin
+            query = ''
+            params.each_with_index do |(key, value), index|
+              query += "#{key} like '#{value}'" if normal_fields.include?(key)
+              query += "#{key} like '%#{value}%'" if like_fields.include?(key)
+              query += " AND " if params.size-1 != index
+            end
+            {"status" => "ok", "result" => Subject.where(query)}
 
-          puts Subject.where(query)
+          rescue
+            {"status" => "error"}
+          end
         end
       end
     end
